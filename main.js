683e76dea2acceb2b67dfc84b2656b1620ed2cd3
OzodGulomjonov <102744037+OzodGulomjonov@users.noreply.github.com>
2022-07-22 15:19:09 +0500

    ozod

---
+++ main.js
@@ -0,0 +1,12 @@
+// let splicedArray = pokemons.splice(0, 10);
+
+// let elList = document.querySelector(".list");
+
+// function render(array) {
+    
+//     for (let i = 0; i < array.length; i++) {
+//         let elLi = document.querySelector(".item")
+
+
+//     }
+// }
